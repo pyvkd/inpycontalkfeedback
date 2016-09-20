91a985c6599c07afc1757998912b727d250fb68f
Vinay Dahiya <vinaykrdahiya@gmail.com>
2016-09-21 04:35:42 +0530

    First Cut

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import falcon
 import sqlite3
-import json
 
 from jinja2 import Environment, PackageLoader
-# import requests
 import datetime
 
 
@@ -19,34 +17,77 @@ class Home:
     """
     def on_get(self, req, resp):
         resp_status = None
-        if req.params.get('time', None):
-            nows = req.params.get('time')
+        master_response = {}
+        if req.get_param('time', None):
+            nows = req.get_param('time')
         else:
             nows = str(datetime.datetime.now() - datetime.timedelta(minutes=5))
-        con = sqlite3.connect('example.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-        Query1 = """SELECT id, title, speaker FROM Talk WHERE start_time <= Datetime(?) AND end_time >= Datetime(?)"""
+        con = sqlite3.connect('pyconfeedback.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+        Query1 = """SELECT id, title, speaker, room FROM Talk WHERE start_time <= Datetime(?) AND end_time >= Datetime(?)"""
         Query1data = (nows, nows)
-        Query2 = """SELECT id, titlte, speaker FROM Talk"""
+        Query2 = """SELECT id, title, speaker, room FROM Talk"""
         try:
             with con:
                 cur = con.execute(Query1, Query1data)
                 master_response['current_talks'] = cur.fetchall()
                 cur = con.execute(Query2)
-                master_response['all_talks'] = cur.fetchall()
+                l = cur.fetchall()
+                master_response['all_talks'] = [x for x in l if (('Tea' not in x[1]) and (x[1]) and ('Breakfast' not in x[1]) and ('Lunch' not in x[1]) and ('Feedback' not in x[1]))]
                 resp_status = falcon.HTTP_200
         except Exception as e:
             master_response['error'] = "Error %s. Kindly Inform about this to any volunteer @ Pycon India 2016." % str(e)
             resp_status = falcon.HTTP_502
         template = env.get_template('index.html')
         resp.status = resp_status
+        resp.body = template.render(master_response=master_response)
+        resp.content_type = "text/html"
 
 
 class Feedback:
-    def on_get():
-        pass
+    """
+    Shows a form for a talk specific feedback
+    """
+    def on_get(self, req, resp, talk_id):
+        resp_status = None
+        master_response = {}
+        Query1 = """SELECT title, speaker, room FROM Talk WHERE id = %d""" % int(talk_id)
+        con = sqlite3.connect('pyconfeedback.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+        try:
+            with con:
+                cur = con.execute(Query1)
+                master_response['item'] = cur.fetchone()
+                master_response['talk_id'] = talk_id
+                print master_response
+                resp_status = falcon.HTTP_200
+        except Exception as e:
+            master_response['error'] = "Error %s. Kindly Inform about this to any volunteer @ Pycon India 2016." % str(e)
+            resp_status = falcon.HTTP_502
+        template = env.get_template('talk.html')
+        resp.status = resp_status
+        resp.body = template.render(master_response=master_response)
+        resp.content_type = "text/html"
 
-    def on_post():
-        pass
+    def on_post(self, req, resp, talk_id):
+        resp_status = None
+        master_response = {}
+        print req.params
+        Query1data = (req.get_param('email'), req.get_param_as_int('rating'), str(datetime.datetime.now()), int(talk_id), req.get_param('comment') )
+        master_response = {}
+        print Query1data
+        Query1 = """INSERT INTO feedback(email, rating, created_on, talk_id, comment) values(?, ?, ?, ?, ?)"""
+        con = sqlite3.connect('pyconfeedback.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+        try:
+            with con:
+                con.execute(Query1, Query1data)
+                con.commit()
+                resp_status = falcon.HTTP_200
+        except Exception as e:
+            master_response['error'] = "Error %s. Kindly Inform about this to any volunteer @ Pycon India 2016." % str(e)
+            resp_status = falcon.HTTP_502
+        template = env.get_template('talkpost.html')
+        resp.status = resp_status
+        resp.body = template.render(master_response=master_response)
+        resp.content_type = "text/html"
 
 
 class AdminTalk:
@@ -57,14 +98,16 @@ class AdminTalk:
         pass
 
 
+# reqwrapper = falcon.RequestOptions()
+# reqwrapper.auto_parse_form_urlencoded = True
 app = falcon.API()
-
+app.req_options.auto_parse_form_urlencoded = True
 # Routing
 home = Home()
 feedback = Feedback()
 admin_talk = AdminTalk()
 
 app.add_route('/', home)
-app.add_route('/talk/<id>/', feedback)
+app.add_route('/talk/{talk_id}/', feedback)
 app.add_route('/admin/talk/', admin_talk)
-app.add_route('/admin/talk/<id>/', admin_talk)
+app.add_route('/admin/talk/{talk_id}/', admin_talk)
