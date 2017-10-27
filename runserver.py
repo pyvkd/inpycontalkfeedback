745c8c3eaa32782285f3f431308c966da7fb29b2
Vinay Kumar Dahiya <vinay.not.nice@gmail.com>
2017-10-27 16:01:20 +0530

    Updating to python3.6 and starting to work for inpycon2017

--- runserver.py
+++
@@ -1,7 +0,0 @@
-from app import app
-# from werkzeug.serving import run_simple
-from wsgiref.simple_server import make_server
-
-if __name__ == '__main__':
-    httpd = make_server('0.0.0.0', 5000, app)
-    httpd.serve_forever()
